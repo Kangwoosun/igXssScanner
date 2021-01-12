8e7cf7fe5e62898007fb9be0a6dae084b766509c
Kangwoosun <kws981924@gmail.com>
2021-01-12 18:10:41 +0900

    test to conflict 2

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import collections
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                       for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+deck = FrenchDeck()
+for card in sorted(deck, key=spades_high):
+    print(card)
